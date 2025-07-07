0acde557b6a5d61b57b794fdfdc1de9f8b396bf0
Rajmate16 <97882958+Rajmate16@users.noreply.github.com>
2025-07-07 18:38:49 +0530

    Update outputs.tf with all infrastructure outputs

--- outputs.tf
+++ outputs.tf
@@ -13,27 +13,52 @@ output "private_subnet_ids" {
   value       = module.vpc.private_subnet_ids
 }
 
-output "db_endpoint" {
+output "database_endpoint" {
   description = "Endpoint of the RDS database"
   value       = module.rds.db_endpoint
 }
 
-output "db_name" {
+output "database_name" {
   description = "Name of the database"
-  value       = var.db_name
+  value       = module.rds.db_name
 }
 
-output "backend_url" {
+output "database_username" {
+  description = "Username for the database"
+  value       = module.rds.db_username
+}
+
+output "database_secret_arn" {
+  description = "ARN of the Secrets Manager secret containing database credentials"
+  value       = aws_secretsmanager_secret.db_credentials.arn
+}
+
+output "backend_api_url" {
   description = "URL of the backend API"
-  value       = module.elastic_beanstalk.api_endpoint
+  value       = "https://${module.elastic_beanstalk.api_endpoint}"
+}
+
+output "backend_environment_name" {
+  description = "Name of the Elastic Beanstalk environment"
+  value       = module.elastic_beanstalk.environment_name
 }
 
 output "frontend_url" {
   description = "URL of the frontend application"
-  value       = module.amplify.app_url
+  value       = module.amplify.production_url
 }
 
-output "db_credentials_secret_arn" {
-  description = "ARN of the secret containing database credentials"
-  value       = aws_secretsmanager_secret.db_credentials.arn
+output "frontend_development_url" {
+  description = "URL of the frontend development environment"
+  value       = module.amplify.development_url
+}
+
+output "amplify_app_id" {
+  description = "ID of the Amplify app"
+  value       = module.amplify.app_id
+}
+
+output "amplify_default_domain" {
+  description = "Default domain for the Amplify app"
+  value       = module.amplify.default_domain
 }
